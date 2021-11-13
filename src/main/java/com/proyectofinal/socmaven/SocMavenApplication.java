3519566a8b5c62a89104b8450ae44aa60ea808d4
Carlos Alberto Londoño Loaiza <carloslondono.co@gmail.com>
2021-11-12 20:29:09 -0500

    primer commit

---
+++ src/main/java/com/proyectofinal/socmaven/SocMavenApplication.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.socmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectofinal/socmaven/SocMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.socmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
